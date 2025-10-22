7459c0174896d4f0771b01a60e4c643dc39c1911
Rodolfo Gonzalez <rodogonzalez.cr@gmail.com>
2025-10-21 19:38:49 -0600

    php index generator qr

---
+++ dist/qr-image/index.php
@@ -0,0 +1,30 @@
+<?php 
+// Register the Composer autoloader...
+require __DIR__.'/vendor/autoload.php';
+
+
+use chillerlan\QRCode\QRCode;
+use chillerlan\QRCode\QROptions;
+
+function generate_qr($string_to_generate){
+
+
+    $options  = new QROptions(
+            [
+                'eccLevel'   => QRCode::ECC_L,
+                'outputType' => QRCode::OUTPUT_MARKUP_SVG,
+                'version'    => QRCode::VERSION_AUTO,
+            ]
+        );        
+        
+        return  (new QRCode($options))->render($string_to_generate);
+
+}
+
+if (isset($_GET['data'])){
+
+    exit (generate_qr($_GET['data']));
+
+}
+
+exit (generate_qr($_GET['data']));
