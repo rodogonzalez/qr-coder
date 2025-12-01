607571180b7b28614f15938c8cd21d32f4cb347d
Rodolfo Gonzalez <rodogonzalez.cr@gmail.com>
2025-12-01 15:06:15 -0600

    last updates

--- dist/qr-image/index.php
+++ dist/qr-image/index.php
@@ -5,6 +5,8 @@ require __DIR__.'/vendor/autoload.php';
 
 use chillerlan\QRCode\QRCode;
 use chillerlan\QRCode\QROptions;
+use chillerlan\QRCode\QRGdImageWEBP;
+
 
 function generate_qr($string_to_generate){
 
@@ -12,16 +14,29 @@ function generate_qr($string_to_generate){
     $options  = new QROptions(
             [
                 'eccLevel'   => QRCode::ECC_L,
-                'outputType' => QRCode::OUTPUT_MARKUP_SVG,
+                'outputType' => QRGdImageWEBP::class,
+                
+
                 'version'    => QRCode::VERSION_AUTO,
             ]
-        );        
+        );
         
-        return  (new QRCode($options))->render($string_to_generate);
+        
+        $gdImage = new QRCode($options))->render($string_to_generate));
+
+        //$gdImage = (new QRCode($options))->render($data);
+
+        // do stuff with the GdImage instance...
+        $size = imagesx($gdImage);
+
+        header('Content-type: image/jpeg');
+
+        imagejpeg($gdImage);
+        imagedestroy($gdImage);
 
 }
 
-header('content-type: image/svg+xml');
+//header('content-type: image/svg+xml');
 
 
 if (isset($_GET['data'])){
