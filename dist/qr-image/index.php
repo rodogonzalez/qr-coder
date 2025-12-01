646b5fa55feadd4bb43c171ee8b997719561e4c2
Rodolfo Gonzalez <rodogonzalez.cr@gmail.com>
2025-12-01 15:25:25 -0600

    last updates

--- dist/qr-image/index.php
+++ dist/qr-image/index.php
@@ -5,18 +5,33 @@ require __DIR__ . '/vendor/autoload.php';
 
 use chillerlan\QRCode\QRCode;
 use chillerlan\QRCode\QROptions;
-
+use chillerlan\QRCode\Data\QRMatrix;
+use chillerlan\QRCode\Output\QRMarkupSVG;
 function generate_qr( $string_to_generate )
 {
 
 
-    $options = new QROptions(
+    $options_old = new QROptions(
         [
             'eccLevel'   => QRCode::ECC_L,
             'outputType' => QRCode::OUTPUT_MARKUP_SVG,
             'version'    => QRCode::VERSION_AUTO,
         ]
     );
+
+    $options = new QROptions;
+
+    $options->version              = 7;
+    $options->outputInterface      = QRMarkupSVG::class;
+    $options->outputBase64         = false;
+    // if set to false, the light modules won't be rendered
+    $options->drawLightModules     = true;
+    $options->svgUseFillAttributes = false;
+    // draw the modules as circles isntead of squares
+    $options->drawCircularModules  = true;
+    $options->circleRadius         = 0.4;
+    // connect paths
+    $options->connectPaths         = true;
 
     $out = (new QRCode($options))->render($string_to_generate);
 
