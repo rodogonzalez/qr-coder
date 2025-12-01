8d0c43d9e0f81f84a1df670361658609a622c21d
Rodolfo Gonzalez <rodogonzalez.cr@gmail.com>
2025-12-01 15:22:26 -0600

    last updates

--- dist/qr-image/index.php
+++ dist/qr-image/index.php
@@ -1,31 +1,39 @@
-<?php 
+<?php
 // Register the Composer autoloader...
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 
 use chillerlan\QRCode\QRCode;
 use chillerlan\QRCode\QROptions;
 
-function generate_qr($string_to_generate){
+function generate_qr( $string_to_generate )
+{
 
 
-    $options  = new QROptions(
-            [
-                'eccLevel'   => QRCode::ECC_L,
-                'outputType' => QRCode::OUTPUT_MARKUP_SVG,
-                'version'    => QRCode::VERSION_AUTO,
-            ]
-        );        
-        
-        return  (new QRCode($options))->render($string_to_generate);
+    $options = new QROptions(
+        [
+            'eccLevel'   => QRCode::ECC_L,
+            'outputType' => QRCode::OUTPUT_MARKUP_SVG,
+            'version'    => QRCode::VERSION_AUTO,
+        ]
+    );
+
+    $out = (new QRCode($options))->render($string_to_generate);
+
+    if (extension_loaded('zlib')) {
+        header('Vary: Accept-Encoding');
+        header('Content-Encoding: gzip');
+        $out = gzencode($out, 9);
+    }
+
+    exit($out);
 
 }
-	header('Content-type: image/svg+xml');
 
-if (isset($_GET['data'])){
+header('Content-type: image/svg+xml');
 
-    exit (generate_qr($_GET['data']));
-
+if (isset($_GET['data'])) {
+    exit(generate_qr($_GET['data']));
 }
 
-exit (generate_qr($_GET['data']));
+exit(generate_qr($_GET['data']));
