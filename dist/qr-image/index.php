701b5495419c370c0e65989bf9d6972362949cbe
Rodolfo Gonzalez <rodogonzalez.cr@gmail.com>
2025-12-01 15:12:17 -0600

    last updates

--- dist/qr-image/index.php
+++ dist/qr-image/index.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 // Register the Composer autoloader...
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 
 use chillerlan\QRCode\QRCode;
@@ -8,36 +8,37 @@ use chillerlan\QRCode\QROptions;
 use chillerlan\QRCode\QRGdImageWEBP;
 
 
-function generate_qr($string_to_generate){
+function generate_qr( $string_to_generate )
+{
 
 
-    $options  = new QROptions(
-            [
-                'eccLevel'   => QRCode::ECC_L,
-                'outputType' => QRGdImageWEBP::class,               
-                'version'    => QRCode::VERSION_AUTO,
-            ]
-        );
-        
-        
-        $gdImage = new QRCode($options)->render($string_to_generate);
-        
-        $size = imagesx($gdImage);
+    $options = new QROptions(
+        [
+            'eccLevel'   => QRCode::ECC_L,
+            'outputType' => QRGdImageWEBP::class,
+            'version'    => QRCode::VERSION_AUTO,
+        ]
+    );
 
-        header('Content-type: image/jpeg');
 
-        imagejpeg($gdImage);
-        imagedestroy($gdImage);
+    $gdImage = (new QRCode($options))->render($string_to_generate);
+
+    //$size = imagesx($gdImage);
+
+    header('Content-type: image/jpeg');
+
+    imagejpeg($gdImage);
+    imagedestroy($gdImage);
 
 }
 
 //header('content-type: image/svg+xml');
 
 
-if (isset($_GET['data'])){
+if (isset($_GET['data'])) {
 
-    exit (generate_qr($_GET['data']));
+    exit(generate_qr($_GET['data']));
 
 }
 
-exit (generate_qr($_GET['data']));
+exit(generate_qr($_GET['data']));
